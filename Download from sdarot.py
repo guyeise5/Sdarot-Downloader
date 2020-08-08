226d422c649d778c9f80adaaa62ca97a7179f094
Guy Eisenbach <guyeise5@gmail.com>
2020-08-08 15:15:10 +0300

    Generic path

--- Download from sdarot.py
+++ Download from sdarot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
-driver.get("https://www.sdarot.work/watch/4961-אדמה-מרה-bir-zamanlar-çukurova")
+driver.get("")
 eps = driver.find_element_by_xpath("/html/body/div[2]/section[1]/div[2]/div/div[2]/ul[2]/li[1]")
 eps.click()
 sleep(30)
