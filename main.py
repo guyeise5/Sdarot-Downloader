603830433dbec2f432b5f6ce9435cb30863b9de7
Maayan Mordehai <maayanmordehai3@gmail.com>
2020-08-11 22:58:22 +0300

    added argumant for the show to download - need to add more in the futuer

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 
 import sdarot.client as client
 import sdarot.show as show
+import argparse
 
-SHOW_ID = 110
+def parse_args():
+    """Parse program arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-s',
+        '--sid',
+        '--show-id',
+        required=True,
+        type=int,
+        help='The show id for the show you want to download',
+    )
+    args = parser.parse_args()
+    return args
 
 def main():
+    parameters = parse_args()
     c = client.Client()
-    s = show.Show(SHOW_ID, c)
+    s = show.Show(parameters.sid, c)
     c.cookie = s.get_cookie()
     s.download(c)
 
