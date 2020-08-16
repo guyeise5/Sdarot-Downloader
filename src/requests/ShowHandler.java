0c2c07d27d2f12917da18c11a6bf9db7e67da8dd
Maayan Mordehai <maayanmordehai3@gmail.com>
2020-08-16 20:58:29 +0300

    added get page func, implemented isExists

--- src/requests/ShowHandler.java
+++ src/requests/ShowHandler.java
@@ -3,6 +3,9 @@ package requests;
 import java.awt.Image;
 import java.io.IOException;
 import java.util.List;
+
+import javax.security.auth.login.Configuration;
+
 import requests.Configurations;
 
 import java.net.URI;
@@ -10,7 +13,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import models.Model;
 import models.Root;
 import models.Show;
 
@@ -33,26 +35,34 @@ public class ShowHandler extends Handler<Root,Show> {
 		return null;
 	}
 	
-	private String getShowName(int ShowID) {
+	private static String getShowName(int ShowID) {
 		// TODO: implement this function
 		// for now - need to think on how to get real name
 		return (String.format("%s", ShowID));
 	}
 	
-	private boolean IsExists(int showID) {
+	private static boolean IsExists(int showID) {
+		try {
+			HttpResponse response = getPageResponse(showID);
+			if (response.statusCode() == Configurations.getInstance().OK_STATUS) {
+	        	return true;
+	        } 
+	        return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+
+	public static HttpResponse getPageResponse(int showID) throws IOException, InterruptedException {
 		HttpClient client = Configurations.getInstance().getHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(String.format("%s/watch/%s", Configurations.getInstance().getSdarotURI().toString(), showID)))
                 .setHeader("User-Agent", Configurations.getInstance().getUserAgent())
                 .build();
-		try {
-			HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
-	        return (response.statusCode() == Configurations.getInstance().OK_STATUS);
-	    } catch (IOException | InterruptedException e1) {
-			e1.printStackTrace();
-			return false;
-		}
+		return client.send(request, HttpResponse.BodyHandlers.ofString());
 	}
 
 	@Override
@@ -66,7 +76,6 @@ public class ShowHandler extends Handler<Root,Show> {
 		if(!IsExists(showID)) {
 			return null;
 		}
-		
 		Show ret = new Show((Root)root, showID);
 		SeasonHandler.getInstance().getAll(ret).forEach(s -> ret.AddChildren(s));
 		ret.SetName(getShowName(showID));
