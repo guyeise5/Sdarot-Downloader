9dbda5787198e61f22cb9f2041a9430f2f83f329
Maayan Mordehai <maayanmordehai3@gmail.com>
2020-08-21 16:50:30 +0300

    not working yet

--- src/requests/EpisodeHandler.java
+++ src/requests/EpisodeHandler.java
@@ -1,16 +1,23 @@
 package requests;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 import models.Episode;
 import models.Model;
@@ -52,58 +59,73 @@ public class EpisodeHandler extends Handler<Season, Episode> {
         while (matcher.find()) {
         	String episodeurl = matcher.group();
         	episodeurl = episodeurl.substring(0, episodeurl.length() - 1); // removing the " in the end
+        	System.out.println(episodeurl);
     		episodes.add(new Episode(season, Integer.parseInt(episodeurl.split("/episode/")[1])));
         }
 		return episodes;
 	}
 	
 	public void download(Episode e)  {
-		// TODO: Implement this function
-		Map<String, Object> vidInfo = getVideoData(e, preWatch(e));
-        String data = String.format("token=%s&time=%s&uid=''", ((Map<String, Object>)vidInfo.get("watch")).get("480"), vidInfo.get("time"));
-        
-        HttpRequest request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(data))
-                .uri(conf.getWatchURI())
-                .setHeader("User-Agent", conf.getUserAgent())
-                .setHeader("Referer", String.format("%s%s", conf.getSdarotURI().toString(), getChangePartUrl((Season)e.getFather(), e.getID())))
-                //.setHeader("X-Requested-With", conf.X_REQUESTED_WITH)
-                .setHeader("Content-Type", conf.CONTENT_TYPE)
-                .build();
-        // TODO: get the response in blocks to file
+		// TODO: Find a better way to deal with error 
+		try {
+			Map<String, Object> vidInfo = getVideoData(e, preWatch(e));
+	        String data = String.format("token=%s&time=%s&uid=''", ((Map<String, Object>)vidInfo.get("watch")).get("480"), vidInfo.get("time"));
+	        
+	        HttpRequest request = HttpRequest.newBuilder()
+	                .GET()
+//	                .POST(HttpRequest.BodyPublishers.ofString(data))         video_url = f"https://{video_info['url']}/w/episode/{self._show_id}/480/{video_info['VID']}.mp4"
+	                .uri(URI.create(String.format("https://%s/w/episode/%s/480/%s.mp4?%s", vidInfo.get("url"), e.getFather().getFather().getID(), vidInfo.get("VID"), data)))
+	                .setHeader("User-Agent", conf.getUserAgent())
+	                .setHeader("Referer", String.format("%s/watch/%s", conf.getSdarotURI().toString(), getChangePartUrl((Season)e.getFather(), e.getID())))
+	                //.setHeader("X-Requested-With", conf.X_REQUESTED_WITH)
+	                .setHeader("Content-Type", conf.CONTENT_TYPE)
+	                .build();
+	        System.out.println("downloading ?");
+	        HttpResponse<InputStream> response = conf.getHttpClient().send(request, HttpResponse.BodyHandlers.ofInputStream());
+	        
+	        File targetFile = new File("ep.mp4");
+	        OutputStream outStream = new FileOutputStream(targetFile);
+	     
+	        byte[] buffer = new byte[8 * 1024];
+	        int bytesRead;
+	        while ((bytesRead = response.body().read(buffer)) != -1) {
+	            outStream.write(buffer, 0, bytesRead);
+	        }
+	        response.body().close();
+	        outStream.close();
+	    
+	        System.out.println();
+	        
+		} catch (IOException | InterruptedException e1) {
+			System.out.printf("Could not download season %s, episode %s%n", e.getFather().getID(), e.getID());
+			e1.printStackTrace();
+		}
 		throw new UnsupportedOperationException("need to implement this");
 
 	}
 	
-	private String preWatch(Episode e) {
+	private String preWatch(Episode e) throws IOException, InterruptedException {
 		String token = null;
 		
 		// request data
         String data = String.format("preWatch=true&season=%s&ep=%s&SID=%s",e.getFather().getID(), e.getID(), e.getFather().getFather().getID());
-        
+        System.out.println(String.format("%s/watch/%s", conf.getSdarotURI().toString(), getChangePartUrl((Season)e.getFather(), e.getID())));
         HttpRequest request = HttpRequest.newBuilder()
                 .POST(HttpRequest.BodyPublishers.ofString(data))
                 .uri(conf.getWatchURI())
                 .setHeader("User-Agent", conf.getUserAgent())
-                .setHeader("Referer", String.format("%s%s", conf.getSdarotURI().toString(), getChangePartUrl((Season)e.getFather(), e.getID())))
+                .setHeader("Referer", String.format("%s/watch/%s", conf.getSdarotURI().toString(), getChangePartUrl((Season)e.getFather(), e.getID())))
                 .setHeader("X-Requested-With", conf.X_REQUESTED_WITH)
                 .setHeader("Content-Type", conf.CONTENT_TYPE)
                 .build();
-
-		try {
-			HttpResponse<String> response = conf.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-	        token = response.body();
-	        // waiting for the token to be valid
-	        Thread.sleep(30000);
-	    
-		} catch (IOException | InterruptedException e1) {
-			System.out.printf("Can't download episode %s failed in prewatch, error:%n", e.getID());
-			e1.printStackTrace();
-		}
+		HttpResponse<String> response = conf.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        token = response.body();
+        // waiting for the token to be valid
+        Thread.sleep(30000);
 		return token;
 	}
 	
-	private Map<String, Object> getVideoData(Episode e, String token) {
+	private Map<String, Object> getVideoData(Episode e, String token) throws IOException, InterruptedException {
 		Map<String, Object> map = null;
 		
 		// request 
@@ -112,21 +134,16 @@ public class EpisodeHandler extends Handler<Season, Episode> {
                 .POST(HttpRequest.BodyPublishers.ofString(data))
                 .uri(conf.getWatchURI())
                 .setHeader("User-Agent", conf.getUserAgent())
-                .setHeader("Referer", String.format("%s%s", conf.getSdarotURI().toString(), getChangePartUrl((Season)e.getFather(), e.getID())))
+                .setHeader("Referer", String.format("%s/watch/%s", conf.getSdarotURI().toString(), getChangePartUrl((Season)e.getFather(), e.getID())))
                 .setHeader("X-Requested-With", conf.X_REQUESTED_WITH)
                 .setHeader("Content-Type", conf.CONTENT_TYPE)
                 .build();
-		try {
-			HttpResponse<String> response = conf.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-	        map = jsonStringToHashMap(response.body());
-	        
-		} catch (IOException | InterruptedException e1) {
-			System.out.printf("Can't download episode %s failed in prewatch, error:%n", e.getID());
-			e1.printStackTrace();
-		}
+		HttpResponse<String> response = conf.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        map = jsonStringToHashMap(response.body());
 		return map;
 	}
 	
+	// TODO: think of where to put this maybe handler or a util class
 	private Map<String, Object> jsonStringToHashMap(String s){
 		Map<String, Object> map = new HashMap<>();
     	Object value;
