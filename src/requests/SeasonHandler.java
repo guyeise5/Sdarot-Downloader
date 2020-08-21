65dafb1a22a29434aa794d97d36638e909d0b31c
Guy Eisenbach <guyeise5@gmail.com>
2020-08-21 23:41:43 +0300

    1. Change name of method 2. Removed unnecessary casting 3. Removed unused variable 4. Remove unused libraries

--- src/requests/SeasonHandler.java
+++ src/requests/SeasonHandler.java
@@ -1,18 +1,10 @@
 package requests;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import models.Episode;
-import models.Model;
-import models.Root;
 import models.Season;
 import models.Show;
 
@@ -31,8 +23,8 @@ public class SeasonHandler extends Handler<Show,Season> {
 	}
 	
 	@Override
-	public String getChangePartUrl(Show show, int seasonID) {
-		return String.format("%s/season/%s", ShowHandler.getInstance().getChangePartUrl(show.getFather(), show.getID()), seasonID);
+	public String getSuffixUrl(Show show, int seasonID) {
+		return String.format("%s/season/%s", ShowHandler.getInstance().getSuffixUrl(show.getFather(), show.getID()), seasonID);
 	}
 	
 	@Override
@@ -40,7 +32,7 @@ public class SeasonHandler extends Handler<Show,Season> {
 		if(!IsExists(show, seasonID)) {
 			return null;
 		}
-		Season ret = new Season((Show)show, seasonID);
+		Season ret = new Season(show, seasonID);
 		EpisodeHandler.getInstance().getAll(ret).forEach(e -> ret.AddChildren(e));
 		return ret;
 	}
@@ -49,9 +41,9 @@ public class SeasonHandler extends Handler<Show,Season> {
 	@Override
 	public List<Season> getAll(Show show) throws IOException, InterruptedException {
 		List<Season> seasons = new ArrayList<>();
-		HttpClient client = conf.getHttpClient();
 		Pattern seasonPattern = Pattern.compile(String.format("(/watch/%s-.*?/season/(\\d+?)\")", show.getID()));
         Matcher matcher = seasonPattern.matcher(ShowHandler.getInstance().getPageResponse(show.getFather(), show.getID()).body());
+        
         while (matcher.find()) {
         	String seasonurl = matcher.group();
         	seasonurl = seasonurl.substring(0, seasonurl.length() - 1); // removing the " in the end
