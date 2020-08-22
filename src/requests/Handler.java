35c7bd2e036c6983166960caf1e436a96dc28669
Maayan Mordehai <maayanmordehai3@gmail.com>
2020-08-22 21:08:28 +0300

    added get all to be in handler - generic function

--- src/requests/Handler.java
+++ src/requests/Handler.java
@@ -6,16 +6,30 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import models.Model;
 
 @SuppressWarnings("rawtypes")
 public abstract class Handler <Father extends Model, T extends Model> {
 
+	private String uriPrefix = "";
+	
 	protected Configurations conf;
 	
 	protected Handler() {
 		this.conf = Configurations.getInstance();
+	}
+	
+	public String getUriPrefix() {
+		return uriPrefix;
+	}
+	
+	protected void setUriPrefix(String uriPre) {
+		uriPrefix = uriPre;
 	}
 	
 	public boolean IsExists(Father father, int ID) {
@@ -29,19 +43,37 @@ public abstract class Handler <Father extends Model, T extends Model> {
 				e.printStackTrace();
 			}
 			return false;
-		
 	}
 	
 	public HttpResponse<String> getPageResponse(Father father, int ID) throws IOException, InterruptedException {
 		HttpClient client = conf.getHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create(String.format("%s/watch/%s", conf.getSdarotURI(), getSuffixUrl(father, ID))))
+                .uri(URI.create(String.format("%s%s", conf.getSdarotURI(), getSuffixUrl(father, ID))).normalize())
                 .setHeader("User-Agent", conf.getUserAgent())
                 .build();
 		return client.send(request, HttpResponse.BodyHandlers.ofString());
 	}
 	
+	public List<T> getAll(Father father) throws IOException, InterruptedException {
+		List<T> models = new ArrayList<>();
+		Pattern modelPattern = getPattern(father);
+		Matcher matcher = modelPattern.matcher(getFatherPageResponse(father).body());
+        while (matcher.find()) {
+        	String modelurl = matcher.group();
+        	modelurl = modelurl.substring(0, modelurl.length() - 1); // removing the " in the end
+        	T model = getByID(father, Integer.parseInt(modelurl.split(getUriPrefix())[1]));
+        	if (model != null) {
+        		models.add(model);        		
+        	}
+        }
+		return models;
+	}
+
+	protected abstract HttpResponse<String> getFatherPageResponse(Father father) throws IOException, InterruptedException;
+	
+	public abstract Pattern getPattern(Father father);
+
 	/**
 	 * <H1>
 	 * getSuffixUrl
@@ -54,10 +86,8 @@ public abstract class Handler <Father extends Model, T extends Model> {
 	 * 
 	 */
 	public abstract String getSuffixUrl(Father father, int ID);
-	
+		
 	public abstract T getByID(Father father, int ID) throws IOException, InterruptedException;
-	
-	public abstract List<T> getAll(Father father) throws IOException, InterruptedException;
 	
 	public abstract void download(T model);
 }
